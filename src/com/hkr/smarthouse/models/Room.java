5240d649abf829ef395e08b2b3dac8c36227584f
Samuil Dragnev <samuil.dragnev@gmail.com>
2014-11-02 01:07:03 +0100

    I've added ORM with Hibernate.

--- src/com/hkr/smarthouse/models/Room.java
+++ src/com/hkr/smarthouse/models/Room.java
@@ -1,32 +1,29 @@
 package com.hkr.smarthouse.models;
 
+import java.util.ArrayList;
 import java.util.List;
 
+import javax.persistence.*;
 import javax.xml.bind.annotation.XmlRootElement;
 
+@Entity
+@Table(name = "Rooms")
 @XmlRootElement
 public class Room {
-	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "roomId", unique = true, nullable = false)
 	private int id;
+	@Column(name = "roomName")
 	private String name;
-	private List<Device> devices;
+//	@OneToMany
+//	private List<Device> devices = new ArrayList<Device>();
+//	public List<Device> getDevices() { return devices; }
+//	public void setDevices(List<Device> devices) { this.devices = devices; }
+	public Room(){}
+	public int getId() { return id; }
+	public void setId(int id) { this.id = id; }
+	public String getName() { return name; }
+	public void setName(String name) { this.name = name; }
 	
-	public int getId() {
-		return id;
-	}
-	public void setId(int id) {
-		this.id = id;
-	}
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
-	public List<Device> getDevices() {
-		return devices;
-	}
-	public void setDevices(List<Device> devices) {
-		this.devices = devices;
-	}
 }
--- src/com/hkr/smarthouse/resources/HouseResources.java
+++ src/com/hkr/smarthouse/resources/HouseResources.java
@@ -1,28 +1,37 @@
 package com.hkr.smarthouse.resources;
 
-import java.util.ArrayList;
+import java.util.List;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 
-import com.hkr.smarthouse.models.Device;
+import org.hibernate.Query;
+import org.hibernate.Session;
+
+import com.hkr.smarthouse.db.HUtil;
 import com.hkr.smarthouse.models.Room;
+
 @Path("/house")
 public class HouseResources {
 
 	@GET
 	@Path("/rooms")
 	@Produces("application/json")
-	public Room getIt(){
-		Room rm = new Room();
-		ArrayList<Device> devices = new ArrayList<Device>();
-		Device dev = new Device();
-		devices.add(dev);
-		devices.add(dev);
-		rm.setDevices(devices);
-		rm.setId(0);
-		rm.setName("opas");
-		return rm;
+	public Room[] getIt() {
+		Room[] rooms;
+		Session session = HUtil.getSessionFactory().openSession();
+
+		Query q = session.createQuery("FROM Room");
+		
+		@SuppressWarnings("unchecked")
+		List<Room> resultList = q.list();
+		rooms = new Room[resultList.size()];
+		System.out.println("num of employess:" + resultList.size());
+		for (int i = 0; i < resultList.size(); i++) {
+			rooms[i] = resultList.get(i);
+		}
+
+		return rooms;
 	}
 }
